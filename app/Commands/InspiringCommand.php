a78dfbe50e5d2c3fab5d6de82e0ea04182ddf821
Armin Winkler <armin.winkler96@gmail.com>
2019-05-12 21:54:39 +0200

    Initial commit

---
+++ app/Commands/InspiringCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Commands;
+
+use Illuminate\Console\Scheduling\Schedule;
+use LaravelZero\Framework\Commands\Command;
+
+class InspiringCommand extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'inspiring {name=Artisan}';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Display an inspiring quote';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->info('Simplicity is the ultimate sophistication.');
+
+        $MadelineProto = new \danog\MadelineProto\API('session.madeline');
+        $MadelineProto->start();
+
+        $me = $MadelineProto->get_self();
+
+        var_dump($me);
+    }
+
+    /**
+     * Define the command's schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
+     * @return void
+     */
+    public function schedule(Schedule $schedule)
+    {
+        // $schedule->command(static::class)->everyMinute();
+    }
+}
---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $madelinePath = base_path() . DIRECTORY_SEPARATOR . 'bootstrap' . DIRECTORY_SEPARATOR . 'madeline.php';
+
+        if (!file_exists('madeline.php')) {
+            copy('https://phar.madelineproto.xyz/madeline.php', $madelinePath);
+        }
+        require_once $madelinePath;
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
